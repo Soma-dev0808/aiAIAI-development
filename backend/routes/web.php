f85f98746226f2218252f4410f7b7cf64b9c750e
Soma-dev0808 <ikeda.s@sprobe.com>
2020-12-12 11:03:40 +0800

    transfered index file, styles and js files

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,115 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// LP
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Admin page
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/login', function () {
+        return view('auth.login');
+    });
+    
+    Route::get('/register', function() {
+        return view('auth.register');
+    });
+
+    Route::get('/password/reset', function() {
+        return view('auth.passwords.email');
+    });
+
+    Route::get('/password/reset/email', function() {
+        return view('auth.passwords.reset');
+    });
+});
+
+// No auth pages
+Route::group(['prefix' => 'information'], function () {
+
+    Route::get('/company-information', function () {
+        return view('information.company_info');
+    });
+
+    Route::get('/contact-form', function () {
+        return view('information.contact_form');
+    });
+
+    Route::get('/guide', function () {
+        return view('information.guide');
+    });
+    
+    Route::get('/faq', function() {
+        return view('information.faq');
+    });
+    
+    Route::get('/term-of-service', function() {
+        return view('information.term_of_service');
+    });
+    
+     Route::get('/privacy-policy', function() {
+        return view('information.privacy_policy');
+    });
+    
+    Route::get('/scta', function() {
+        return view('information.scta');
+    });
+
+    Route::get('/shop-list', function() {
+        return view('information.shop_list');
+    });
+
+    Route::get('/branch-list', function () {
+        return view('information.branch_list');
+    });
+    
+    Route::get('/news', function () {
+        return view('news.index');
+    });
+
+    Route::get('/news/20201128', function () {
+        return view('news.20201128');
+    });
+
+    Route::get('/search', function () {
+        return view('search');
+    });
+});
+
+// User page
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/dashboard', function () {
+        return view('user');
+    });
+
+    Route::get('/select-shop', function () {
+        return view('shopping.select-shop');
+    });
+
+    Route::get('/category', function () {
+        return view('shopping.category');
+    });
+
+    Route::get('/shopping', function () {
+        return view('shopping.index');
+    });
+
+    Route::get('/cart', function () {
+        return view('shopping.cart');
+    });
+
+    Route::get('/shopping/received', function () {
+        return view('shopping.received');
+    });
+
+    Route::get('/account', function () {
+        return view('auth.account');
+    });
+
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
